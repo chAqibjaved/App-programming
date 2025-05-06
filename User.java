75ea45b36a54f53e69479f7b776da819ac8f0622
Aqib Javed <aqibjaveduog@gmail.com>
2025-05-06 15:21:11 +0200

    Add files via upload

--- User.java
+++ User.java
@@ -16,7 +16,26 @@ public class User {
 
     private String passportId;
 
+    private String insertnewusers;
+    private String access_key;
+
+    public String getAccess_key() {
+        return access_key;
+    }
+
+    public void setAccess_key(String access_key) {
+        this.access_key = access_key;
+    }
+
     private String disease;
+
+    public String getInsertnewusers() {
+        return insertnewusers;
+    }
+
+    public void setInsertnewusers(String insertnewusers) {
+        this.insertnewusers = insertnewusers;
+    }
 
     private String password;
 
