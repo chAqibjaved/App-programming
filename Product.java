e47879af00f4ee411946221d20c0b68945cbf248
Aqib Javed <aqibjaveduog@gmail.com>
2025-05-06 10:18:25 +0200

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,17 @@
+package com.aqib.mymedreminder.model;
+
+public class Product {
+
+
+        private int id;
+        private String title;
+        private String description;
+        private int price;
+
+        // Getters
+        public int getId() { return id; }
+        public String getTitle() { return title; }
+        public String getDescription() { return description; }
+        public int getPrice() { return price; }
+
+}
---
+++ ProductResponse.java
@@ -0,0 +1,11 @@
+package com.aqib.mymedreminder.model;
+
+import java.util.List;
+
+public class ProductResponse {
+    private List<Product> products;
+
+    public List<Product> getProducts() {
+        return products;
+    }
+}
